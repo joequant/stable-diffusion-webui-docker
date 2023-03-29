bde58269d75289b565fe8b7ff7b7780e3e5bb947
Joseph C Wang <joequant@gmail.com>
2023-03-30 00:04:33 +0800

    change to medvram

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=/opt/rocm/lib
 export CUDA_VISIBLE_DEVICES=0
 #export HSA_OVERRIDE_GFX_VERSION=10.3.0
 # Load the torch versions already installed
-python3 -u launch.py  --skip-install --no-half --lowvram --disable-safe-unpickle
+python3 -u launch.py  --skip-install --medvram --disable-safe-unpickle
 
 
 
